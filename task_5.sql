223ecc235a5e00cba7a4b820c2a060ba77df5953
Medrin Kariuki <medrinkariuki@gmail.com>
2025-08-14 10:30:26 +0300

    Add script to insert first customer data

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
-INSERT INTO Customers (CUSTOMER_ID, NAME, EMAIL, ADDRESS)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
